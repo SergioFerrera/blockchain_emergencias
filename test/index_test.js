70f7fd04ab86acdf929506d84f8a75853bf8aaae
Sergio Ferrera de Diego <sergioferrera1296@gmail.com>
2019-03-07 16:22:35 +0000

    Añadiendo testing Google Maps API en index

--- test/index_test.js
+++ test/index_test.js
@@ -82,7 +82,7 @@ describe('Comprobando titulo (h1) y descripción del proyecto', function() {
 describe('Comprobando formulario', function() {
     it('Comprobando campo tipo de emergencia y sus opciones a escoger', function() {
         request('http://localhost:3000' , function(error, response, body) {
-            expect(body).contain('<div class="form-group"><label for="tipo"><strong>Tipo de Emergencia</strong></label>');
+            expect(body).contain('<div class="form-group"><label for="type"><strong>Tipo de Emergencia</strong></label>');
             expect(body).contain('<option>Accidente de Trafico</option>');
             expect(body).contain('<option>Incendio</option>');
             expect(body).contain('<option>Homicidio</option>');
@@ -93,37 +93,57 @@ describe('Comprobando formulario', function() {
     });
     it('Comprobando campo especificar tipo', function() {
         request('http://localhost:3000' , function(error, response, body) {
-            expect(body).contain('<label for="tipo"><strong>Especificar tipo de emergencia</strong></label>');
+            expect(body).contain('<label for="type"><strong>Especificar tipo de emergencia</strong></label>');
+        });
+    });
+    it('Comprobando sección de formulario Ubicación', function() {
+        request('http://localhost:3000' , function(error, response, body) {
+            expect(body).contain('<label for="location"><strong>Ubicación de la emergencia</strong></label>');
+        });
+    });
+    it('Comprobando botón Ubicación Actual', function() {
+        request('http://localhost:3000' , function(error, response, body) {
+            expect(body).contain('<button class="btn btn-secondary" type="button" onclick="getLocation()">Ubicación actual</button>');
+        });
+    });
+    it('Comprobando campo en caso de error con la geolocalización', function() {
+        request('http://localhost:3000' , function(error, response, body) {
+            expect(body).contain('<p id="error_with_geolocation"></p>');
+        });
+    });
+    it('Comprobando input de Google Maps API', function() {
+        request('http://localhost:3000' , function(error, response, body) {
+            expect(body).contain('<input class="form-control form-control-sm" id="pac-input"');
         });
     });
     it('Comprobando campo Latitud', function() {
         request('http://localhost:3000' , function(error, response, body) {
-            expect(body).contain('<label for="latitud"><strong>Latitud</strong></label>');
+            expect(body).contain('<input class="form-control form-control-sm" id="latitude"');
         });
     });
     it('Comprobando campo Longitud', function() {
         request('http://localhost:3000' , function(error, response, body) {
-            expect(body).contain('<label for="longitud"><strong>longitud</strong></label>');
+            expect(body).contain('<input class="form-control form-control-sm" id="longitude"');
         });
     });
     it('Comprobando campo Número de ambulancias', function() {
         request('http://localhost:3000' , function(error, response, body) {
-            expect(body).contain('<label for="ambulancias"><strong>Número de ambulancias</strong></label>');
+            expect(body).contain('<label for="ambulances"><strong>Número de ambulancias</strong></label>');
         });
     });
     it('Comprobando campo Número de camiones de bomberos', function() {
         request('http://localhost:3000' , function(error, response, body) {
-            expect(body).contain('<label for="bomberos"><strong>Número de camiones de bomberos</strong></label>');
+            expect(body).contain('<label for="firefighters"><strong>Número de camiones de bomberos</strong></label>');
         });
     });
     it('Comprobando campo Número de policias', function() {
         request('http://localhost:3000' , function(error, response, body) {
-            expect(body).contain('<label for="policias"><strong>Número de policias</strong></label>');
+            expect(body).contain('<label for="police"><strong>Número de policias</strong></label>');
         });
     });
     it('Comprobando campo Notas', function() {
         request('http://localhost:3000' , function(error, response, body) {
-            expect(body).contain('<label for="notas"><strong>Notas</strong></label>');
+            expect(body).contain('<label for="notes"><strong>Notas</strong></label>');
         });
     });
 });
@@ -131,7 +151,7 @@ describe('Comprobando formulario', function() {
 describe('Comprobando javascript', function() {
     it('Comprobando función para mostrar campo de especificar tipo', function() {
         request('http://localhost:3000' , function(error, response, body) {
-            expect(body).contain('function mostrar_otro()');
+            expect(body).contain('function show_other()');
         });
     });
 });
@@ -153,11 +173,22 @@ describe('Comprobando dependencias', function() {
                 expect(body).contain('<script src="js/popper.js"></script>');
             });
         });
+        it('Comprobando Google Maps API', function() {
+            request('http://localhost:3000' , function(error, response, body) {
+                expect(body).contain('<script src="js/map.min.js"></script>');
+                expect(body).contain('<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAR51eYlvnZn7xXA-4ZCdQW8CnDXTdy8NY&amp;libraries=places&amp;callback=initAutocomplete" async defer></script>');
+            });
+        });
     })
     describe('CSS', function() {
         it('Comprobando bootstrap', function() {
             request('http://localhost:3000' , function(error, response, body) {
                 expect(body).contain('<link rel="stylesheet" href="css/bootstrap.min.css">');
+            });
+        });
+        it('Comprobando Google Maps API', function() {
+            request('http://localhost:3000' , function(error, response, body) {
+                expect(body).contain('<link rel="stylesheet" href="css/map.min.css">');
             });
         });
     })

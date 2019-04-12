0faa25d83678458c96315be69e9a14a1b74d5f98
Sergio Ferrera de Diego <sergioferrera1296@gmail.com>
2019-04-12 10:52:54 +0100

    Obteniendo Abi directamente del fichero json del contrato

--- dist/js/smart_contract.js
+++ dist/js/smart_contract.js
@@ -1,7 +1,7 @@
+var contract = require('../../blockchain-project/build/contracts/EmergencyContract.json')
 var Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
-var abi=JSON.parse('[{"constant":true,"inputs":[],"name":"firefighters","outputs":[{"name":"","type":"int256"}],"payable":false,"stateMutability":"view","type":"function","signature":"0x87267f20"},{"constant":true,"inputs":[],"name":"ambulances","outputs":[{"name":"","type":"int256"}],"payable":false,"stateMutability":"view","type":"function","signature":"0x8d9c28b9"},{"constant":true,"inputs":[],"name":"police","outputs":[{"name":"","type":"int256"}],"payable":false,"stateMutability":"view","type":"function","signature":"0xc7330dd4"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor","signature":"constructor"},{"constant":false,"inputs":[{"name":"n_a","type":"int256"},{"name":"n_f","type":"int256"},{"name":"n_p","type":"int256"}],"name":"set_resources","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function","signature":"0x3754547d"}]')
-var EmergencyContract = web3.eth.contract(abi);
+var EmergencyContract = web3.eth.contract(contract.abi);
 var contractInstance = EmergencyContract.at('0x9EfF89ab6320FE8AB91a4a159bA15A4FC52afbE9');
 
 module.exports = {

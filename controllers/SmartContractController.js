5205c9e9c0764e9d6b8c90f0bd797a9645d940c1
Sergio Ferrera de Diego <sergioferrera1296@gmail.com>
2019-04-21 13:36:37 +0100

    Añadiendo selector de organizaciones receptoras

--- controllers/SmartContractController.js
+++ controllers/SmartContractController.js
@@ -1,15 +1,23 @@
 var smart_contract = require('../dist/js/smart_contract');
+var mysql = require('mysql');
 
 module.exports = {
     getEmergency : function(req, res, next)
     {
         if (req.user.user_type != "Proveedor de recursos")
         {
-            return res.render('solicitud', {
-                message: req.flash('info'),
-                isAuthenticated : req.isAuthenticated(),
-                user : req.user,
-                title: 'Pagina de solicitud de recursos'
+            var config = require('../database/config');
+            var db = mysql.createConnection(config);
+            db.connect();
+            db.query('SELECT DISTINCT organization_name FROM users', function (err, rows, fields) {
+                if (err) throw err;
+                return res.render('solicitud', {
+                    organizations_names : rows,
+                    message: req.flash('info'),
+                    isAuthenticated : req.isAuthenticated(),
+                    user : req.user,
+                    title: 'Pagina de solicitud de recursos'
+                });
             });
         }
         else

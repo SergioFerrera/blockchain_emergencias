fdb1fa6259ef784ad0074732fae4809228061693
Sergio Ferrera de Diego <sergioferrera1296@gmail.com>
2019-04-29 15:35:34 +0100

    Mostrando emergencias en el panel del proveedor

--- controllers/SmartContractController.js
+++ controllers/SmartContractController.js
@@ -1,5 +1,9 @@
 var smart_contract = require('../dist/js/smart_contract');
 var mysql = require('mysql');
+var contract_json = require('../blockchain-project/build/contracts/EmergencyContract.json');
+var Web3 = require('web3');
+var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
+var EmergencyContract = web3.eth.contract(contract_json.abi);
 
 module.exports = {
     getEmergency : function(req, res, next)
@@ -19,6 +23,7 @@ module.exports = {
                     title: 'Pagina de solicitud de recursos'
                 });
             });
+            db.end();
         }
         else
         {
@@ -33,19 +38,44 @@ module.exports = {
             police : req.body.police,
         };
         req.flash('info', 'Se ha enviado correctamente la emergencia');
-        smart_contract.set_resources(resources,req.user);
+        smart_contract.set_resources(resources,req.user,req.body.organizations);
         return res.redirect('/solicitud');
     },
     getContracts : function(req, res, next)
     {
         if (req.user.user_type != "Operador de emergencias")
         {
-            return res.render('emergencias', {
-                message: req.flash('info'),
-                isAuthenticated : req.isAuthenticated(),
-                user : req.user,
-                title: 'Emergencias registradas'
-            });
+            var config = require('../database/config');
+            var db = mysql.createConnection(config);
+            db.connect();
+            var organization_name = req.user.organization_name;
+            db.query('SELECT * FROM contracts', function(err, rows, fields){
+                if (err) throw err;
+                var emergencies = [];
+                var i;
+                rows.forEach(function(i) {
+                    var contractInstance = EmergencyContract.at(i.address);
+                    if (contractInstance.state()==true){
+                        for (j = 0; j < contractInstance.get_organizations_length(); j++){
+                            if (contractInstance.organizations(j) == organization_name){
+                                emergencies.push({id: i.id, ambulances: contractInstance.ambulances(), firefighters: contractInstance.firefighters(), police: contractInstance.police()});
+                            }
+                        };
+                    };
+                });
+                db.query('SELECT * FROM users WHERE id=?', req.user.id, function(err, rows, fields){
+                    req.user.ambulances=rows[0].ambulances;
+                    req.user.firefighters=rows[0].firefighters;
+                    req.user.police=rows[0].police;
+                    return res.render('emergencias', {
+                        emergencies : emergencies,
+                        message: req.flash('info'),
+                        isAuthenticated : req.isAuthenticated(),
+                        user : req.user,
+                        title: 'Emergencias registradas'
+                    });
+                });
+            });       
         }
         else
         {

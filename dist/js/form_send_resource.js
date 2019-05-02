beb6f42ea4090d0750ecee90503a713a133b0139
Sergio Ferrera de Diego <sergioferrera1296@gmail.com>
2019-05-02 13:50:20 +0100

    Minificando código javascript para enviar recursos

--- dist/js/form_send_resource.js
+++
@@ -1,5 +0,0 @@
-function check_available(current){
-    current.setCustomValidity('');
-    if (current.value>Number(document.getElementById('available_'+current.id[9]).textContent))
-        current.setCustomValidity('Excede el número de recursos disponibles');
-}
